7ecbe950735c45167a424c6299046d92cb239abf
alisson-moura <alisson.mo.moura@outlook.com.br>
2022-09-05 14:45:36 -0300

    fix: corrigido estilos dos cards

--- src/pages/Home/Card/style.ts
+++ src/pages/Home/Card/style.ts
@@ -7,6 +7,8 @@ export const Card = styled(Link)`
     color: ${props => props.theme["base-text"]};
     background: ${props => props.theme['base-profile']};
     padding: 32px;
+    height: 200px;
+    min-width: 50%;
     border-radius: ${props => props.theme["radius-10"]};
     cursor: pointer;
     header {
@@ -16,8 +18,10 @@ export const Card = styled(Link)`
         gap: 1rem;
         h3 {
             color: ${props => props.theme['base-title']};
-            display: inline-flex;
             font-size: 24px;
+            white-space: pre;
+            overflow: hidden;
+            text-overflow: ellipsis;
             flex: 1;
         }
         margin-bottom: 20px;
